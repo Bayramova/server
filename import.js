a6a62e9704236651d01f14fed5cf428f5ea1586a
Sabina Bairamava <Sabina.Bairamava@itechart-group.com>
2019-03-22 19:19:42 +0300

    import data from json to database

--- import.js
+++ import.js
@@ -16,42 +16,32 @@ process.argv.forEach(element => {
 });
 const DATA = path.join(__dirname, `./data/${FILE_NAME}`);
 
-let table_exists = false;
-db.sequelize
-  .query(
-    `SELECT * FROM information_schema.TABLES WHERE TABLE_SCHEMA = 'cleaning-services' AND TABLE_NAME = '${TABLE_NAME}';`
-  )
-  .then(([results, metadata]) => {
-    console.log(results);
-    if (results) {
-      table_exists = true;
-      db.sequelize.query(`DELETE FROM ${TABLE_NAME}`);
-    }
-  });
-
-console.log("Table existsts?????????" + table_exists);
-
 fs.readFile(DATA, (err, data) => {
   const records = JSON.parse(data);
   const columns = Object.keys(records[0]);
-  let data_length = records.length;
-
-  //   if (!table_exists) {
-  //     db.sequelize.query(
-  //       `CREATE TABLE ${TABLE_NAME} (${columns.join(" TEXT, ")} TEXT);`
-  //     );
-  //   }
-
-  records.forEach(record => {
-    const values = Object.values(record);
-    let valuesToInsert = '"' + values.join('", "') + '"';
-    db.sequelize.query(`INSERT INTO ${TABLE_NAME} VALUES (${valuesToInsert});`);
-    data_length = data_length - 1;
-    console.log(`Importing... (${data_length}) Record(s)`);
-    console.clear();
-    if (!data_length) {
-      console.log(`Done importing (${records.length}) Records`);
-      process.exit();
-    }
-  });
+  db.sequelize
+    .query(
+      `SELECT COUNT(*) AS count
+    FROM information_schema.tables 
+    WHERE table_schema = 'cleaning-services' 
+    AND table_name = '${TABLE_NAME}';`
+    )
+    .then(data => {
+      if (data[0][0].count == 0) {
+        db.sequelize.query(
+          `CREATE TABLE ${TABLE_NAME} (${columns.join(
+            " VARCHAR(300), "
+          )} VARCHAR(300));`
+        );
+      } else {
+        db.sequelize.query(`DELETE FROM ${TABLE_NAME}`);
+      }
+      records.forEach(record => {
+        const values = Object.values(record);
+        let valuesToInsert = '"' + values.join('", "') + '"';
+        db.sequelize.query(
+          `INSERT INTO ${TABLE_NAME} VALUES (${valuesToInsert});`
+        );
+      });
+    });
 });
