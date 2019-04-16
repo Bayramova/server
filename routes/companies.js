2ddc775175659a54014e477b1c0077307a7cfaba
Sabina <sb.sabina00@gmail.com>
2019-04-17 00:55:51 +0300

    add infinite scroll

--- routes/companies.js
+++ routes/companies.js
@@ -5,35 +5,21 @@ const express = require("express");
 const router = express.Router();
 const Company = require("../models/company");
 
-// router.get("/companies/:page", async (req, res) => {
-//   try {
-//     const limit = 10;
-//     let offset = 0;
-//     const data = await Company.findAndCountAll();
-//     if (data) {
-//       const { page } = req.params;
-//       offset = limit * (page - 1);
-//       const companies = await Company.findAll({
-//         limit,
-//         offset
-//       });
-//       if (companies) {
-//         res.json(companies);
-//       }
-//     }
-//   } catch (err) {
-//     res.status(500).json({
-//       message: "Something went wrong."
-//     });
-//     console.log(`Error: ${err}`);
-//   }
-// });
-
-router.get("/companies", async (req, res) => {
+router.get("/companies/:page", async (req, res) => {
   try {
-    const companies = await Company.findAll();
-    if (companies) {
-      res.json(companies);
+    const limit = 5;
+    let offset = 0;
+    const data = await Company.findAndCountAll();
+    if (data) {
+      const { page } = req.params;
+      offset = limit * (page - 1);
+      const companies = await Company.findAll({
+        limit,
+        offset
+      });
+      if (companies) {
+        res.json(companies);
+      }
     }
   } catch (err) {
     res.status(500).json({
