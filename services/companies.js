cd8c1a02fda596c579c763a194c14ee66e00effe
Sabina Bairamava <Sabina.Bairamava@itechart-group.com>
2019-04-23 21:59:05 +0300

    fix companies route

--- services/companies.js
+++ services/companies.js
@@ -7,7 +7,7 @@
 const Sequelize = require("sequelize");
 const Company = require("../models/company");
 
-const getCompanies = async (params, res) => {
+const getCompanies = async (query, params, res) => {
   try {
     const page = params.page || 1;
     let limit = parseInt(params.limit, 10);
@@ -19,20 +19,32 @@ const getCompanies = async (params, res) => {
       limit = 1;
     }
     let offset = 0;
-    const data = await Company.findAndCountAll();
-    if (data) {
-      offset = limit * (page - 1);
-      const companies = await Company.findAll({
-        limit,
-        offset
-      });
-      if (companies) {
-        const hasMore = page < Math.ceil(data.count / limit);
-        return {
-          companies,
-          hasMore
-        };
+    offset = limit * (page - 1);
+    let companies = await Company.findAndCountAll({
+      limit,
+      offset,
+      where: {
+        [Sequelize.Op.or]: [
+          {
+            name: {
+              [Sequelize.Op.substring]: query
+            }
+          },
+          {
+            services: {
+              [Sequelize.Op.substring]: query
+            }
+          }
+        ]
       }
+    });
+    if (companies) {
+      const hasMore = page < Math.ceil(companies.count / limit);
+      companies = companies.rows;
+      return {
+        companies,
+        hasMore
+      };
     }
   } catch (err) {
     return res.status(500).json({
@@ -58,47 +70,4 @@ const getCompany = async (params, res) => {
   }
 };
 
-const searchCompanies = async (query, params, res) => {
-  try {
-    const page = params.page || 1;
-    let limit = parseInt(params.limit, 10);
-    if (isNaN(limit)) {
-      limit = 5;
-    } else if (limit > 50) {
-      limit = 50;
-    } else if (limit < 1) {
-      limit = 1;
-    }
-    const filteredByName = Company.findAll({
-      where: {
-        name: {
-          [Sequelize.Op.substring]: query
-        }
-      }
-    });
-    const filteredByService = Company.findAll({
-      where: {
-        services: {
-          [Sequelize.Op.substring]: query
-        }
-      }
-    });
-    const filtered = [...filteredByName, ...filteredByService];
-    const companies = filtered.slice(
-      limit * (page - 1),
-      limit * (page - 1) + limit
-    );
-    const hasMore = page < Math.ceil(filtered.length / limit);
-    return {
-      companies,
-      hasMore
-    };
-  } catch (err) {
-    console.log(`Error: ${err}`);
-    return res.status(500).json({
-      message: "Something went wrong."
-    });
-  }
-};
-
-module.exports = { getCompanies, getCompany, searchCompanies };
+module.exports = { getCompanies, getCompany };
