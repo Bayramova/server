43bbef8d44139ed01e14fc03b8fda1999268ccad
Sabina Bairamava <Sabina.Bairamava@itechart-group.com>
2019-04-29 19:12:02 +0300

    edit appspec file

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -3,3 +3,6 @@
 # Stop all servers and start the server
 pm2 stop
 pm2 start /home/ubuntu/server/server.js
+
+# Restart nginx
+sudo service nginx stop && sudo service nginx start
--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -9,8 +9,23 @@ sudo npm cache clean -f
 sudo npm install -g n
 sudo n stable
 
-# Install nodemon
-sudo npm install nodemon -g
+# Install nginx
+sudo apt-get install nginx git -y
+sudo apt-get install -y build-essential openssl libssl-dev pkg-config
+cd /etc/nginx/sites-available
+sudo vim server
+vim -c "server {
+    listen 80;
+    location / {
+        proxy_pass http://10.0.0.232:5000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host $host;
+        proxy_cache_bypass $http_upgrade;
+    }
+}" -c "wq"
+sudo rm default
 
 # Install pm2
 sudo npm install pm2 -g
